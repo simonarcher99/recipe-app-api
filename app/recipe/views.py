76932d0d94671ba89bc08ab886f6ef945c27e24b
Simon Archer <simon.james.archer@gmail.com>
2021-10-21 11:29:49 +0100

    Fixing bug

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -53,7 +53,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
 
-        elif self.action == 'upload-image':
+        elif self.action == 'upload_image':
             return serializers.RecipeImageSerializer
 
         return self.serializer_class
